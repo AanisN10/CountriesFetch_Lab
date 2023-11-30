242ed3175003524e652b1eb46827b9e636dfea12
Aanis Nuur <Aanisn10@gmail.com>
2023-11-30 17:54:04 +0000

    forgot to add the refractored code, made a few changes

--- lab_countries/apps.js
+++ lab_countries/apps.js
@@ -1,53 +1,51 @@
+
 const countryInfo = document.querySelector("#countries-description");
 
+
+  const displayCountryInfo = (country) => {
+
+    const countryDiv = document.createElement("div")
+    const nameOutput = document.createElement("p");
+    const nameInfo = country.name.common;
+    nameOutput.innerText = `Name: ${nameInfo}`;
+    // countryDiv.appendChild(nameOutput);
+
+
+    const populationOutput = document.createElement("p");
+    const populationInfo = country.population;
+    populationOutput.innerText = `Population: ${populationInfo}`;
+    // countryDiv.appendChild(populationOutput);
+
+
+    const languagesOutput = document.createElement("ul");
+    Object.values(country.languages).forEach((language) => {
+    const languagesInfo = document.createElement("li") 
+    languagesInfo.innerText= language;
+    languagesOutput.appendChild(languagesInfo)    
+    });
+    
+    const capitalOutput = document.createElement("p");
+    const capitalInfo = country.capital[0];
+    capitalOutput.innerText = `Capital: ${capitalInfo}`;
+
+    countryDiv.appendChild(nameOutput);
+    countryDiv.appendChild(populationOutput);
+    countryDiv.appendChild(languagesOutput)
+    countryDiv.appendChild(capitalOutput)
+
+  
+   countryInfo.appendChild(countryDiv)
+
+   
+}
 
 const getCountryByName = async (countryName) => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
     const jsonData = await response.json();
-    console.log(jsonData[0]);
-
-
-
-    const nameOutput = document.createElement("p");
-    const nameInfo = jsonData[0].name.common;
-    nameOutput.innerText = `Name: ${nameInfo}`;
-    countryInfo.appendChild(nameOutput);
-
-
-    const populationOutput = document.createElement("p");
-    const populationInfo = jsonData[0].population;
-    populationOutput.innerText = `Population: ${populationInfo}`;
-    countryInfo.appendChild(populationOutput);
-
-    
-    // const languagesOutput = document.createElement("div");
-    // const languagesInfo = jsonData[0].languages;
-
-    // Loop through the values of the languagesInfo object
-    // Object.values(languagesInfo).forEach((language)=>{
-            
-    //     languagesOutput.innerHTML = `Languages ${(Object.values(language)).join(",")} `
-    
-    // })
-
-    // countryInfo.appendChild(languagesOutput)
-
-
-    const languagesOutput = document.createElement("div");
-    const languagesInfo = jsonData[0].languages;
-    // covert object into a json formatted string
-    const convertToJson = JSON.stringify(languagesInfo);
-
-    languagesOutput.innerText = `Languages: ${convertToJson}`;
-    countryInfo.appendChild(languagesOutput);
-
-    const capitalOutput = document.createElement("div");
-    const capitalInfo = jsonData[0].capital;
-    capitalOutput.innerText = `Capital: ${capitalInfo}`;
-    countryInfo.appendChild(capitalOutput);    
-    
+    // the getCountryByName is asynchronous , so have to wait for getCountryByName and then use the results
+    displayCountryInfo(jsonData[0])
+    // console.log(jsonData)
 }
 getCountryByName("Papua New Guinea")
-
 
 
